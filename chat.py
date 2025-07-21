b99553908f8791c32ce266b1da3fc74fe72524c9
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-07-21 13:14:18 +0000

    feat: add user_id to chat.py frontend

--- chat.py
+++ chat.py
@@ -10,8 +10,11 @@ import httpx
 hack_extract = re.compile(r"<!-- session_id: ([a-z0-9]+) -->")
 
 
-async def chat(msg: str, hist: List[gr.MessageDict]):
+async def chat(msg: str, hist: List[gr.MessageDict], user_id: str):
     """Process the message as necessary."""
+    if user_id == "":
+        raise gr.Error("User ID cannot be empty.")
+
     # TODO: backend should validate that it allocated the session_id
     # HACK: Iterate backwards till we find a model message and extract the session_id from it.
     for obj in reversed(hist):
@@ -28,19 +31,21 @@ async def chat(msg: str, hist: List[gr.MessageDict]):
     else:
         print("No session_id found in history, generating a new one.")
         session_id = uuid4().hex
-    print(f"Using session_id: {session_id}")
+    print(f"Using session_id: {session_id}, user_id: {user_id}")
 
     async with httpx.AsyncClient() as client:
         async with client.stream(
             "POST",
             "http://localhost:3000/chat",
-            json={"msg": msg, "session_id": session_id},
+            json={"msg": msg, "session_id": session_id, "user_id": user_id},
         ) as res:
             async for update in res.aiter_bytes():
                 out = update.decode("utf-8")
                 out += f"\n<!-- session_id: {session_id} -->"
                 yield out
 
+
+user_id = gr.Textbox(label="User ID")
 
 demo = gr.ChatInterface(
     fn=chat,
@@ -51,6 +56,7 @@ demo = gr.ChatInterface(
     # editable=True,
     # We can still save smth for frontend, but its not the ground truth.
     save_history=True,
+    additional_inputs=[user_id],
 )
 
 # NOTE: gradio chat.py breaks the chat interface after refresh for some reason.
