810c2ce7918fa0834e01c97d74c113955a3bfaab
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-07-22 05:25:35 +0000

    fix: maintain both RichHandler and FileHandler styles correctly

--- dev.py
+++ dev.py
@@ -7,22 +7,22 @@ from rich.logging import RichHandler
 
 def create_debug_app():
     """Workaround to use different logger for debug."""
+    import contextlib
+
     import fastapi
     import starlette
     import uvicorn
 
     rich_handler = RichHandler(
-        rich_tracebacks=True, tracebacks_suppress=[fastapi, starlette, uvicorn]
+        rich_tracebacks=True,
+        tracebacks_suppress=[fastapi, starlette, uvicorn, contextlib],
     )
     file_handler = logging.FileHandler("dev.log", mode="a", encoding="utf-8")
     file_handler.setFormatter(
         logging.Formatter("[%(asctime)s] %(levelname)s %(name)s: %(message)s")
     )
 
-    logging.basicConfig(
-        handlers=[rich_handler, file_handler],
-        level=logging.NOTSET,
-    )
+    logging.basicConfig(format="", handlers=[rich_handler, file_handler])
     logging.getLogger("src").setLevel(logging.DEBUG)
 
     from src import create_app
@@ -39,10 +39,7 @@ if __name__ == "__main__":
         logging.Formatter("[%(asctime)s] %(levelname)s %(name)s: %(message)s")
     )
 
-    logging.basicConfig(
-        handlers=[rich_handler, file_handler],
-        level=logging.NOTSET,
-    )
+    logging.basicConfig(format="", handlers=[rich_handler, file_handler])
 
     uvicorn.run(
         "dev:create_debug_app",
