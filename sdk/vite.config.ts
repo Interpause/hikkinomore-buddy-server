9de994e37953288e40ac0977485088d6a045bdaa
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-07-24 14:34:17 +0800

    feat(sdk): initial commit of js sdk for server

---
+++ sdk/vite.config.ts
@@ -0,0 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  plugins: [dts({ include: ['lib'] })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'hikkinomore-buddy-sdk',
+      // the proper extensions will be added
+      fileName: 'hikkinomore-buddy-sdk',
+    },
+    copyPublicDir: false,
+  },
+})
